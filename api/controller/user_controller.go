1272f8cd551336cd986a9926efb7f65d9719d50f
Kevin Gomes <kevinmg50@gmail.com>
2023-11-09 16:31:01 -0300

    feat: implemented user login

--- api/controller/user_controller.go
+++ api/controller/user_controller.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kevinfinalboss/FinOps/api/middlewares"
 	"github.com/kevinfinalboss/FinOps/internal/domain"
 	"github.com/kevinfinalboss/FinOps/internal/repository"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -58,4 +59,38 @@ func (uc *UserController) RegisterUser(c *gin.Context) {
 func isValidEmail(email string) bool {
 	regex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
 	return regex.MatchString(email)
+}
+
+func (uc *UserController) LoginUser(c *gin.Context) {
+	var loginCreds struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	if err := c.ShouldBindJSON(&loginCreds); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Dados de entrada inválidos"})
+		return
+	}
+
+	user, err := uc.repo.FindUserByEmail(c, loginCreds.Email)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao buscar usuário"})
+		return
+	}
+	if user == nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Credenciais inválidas"})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(loginCreds.Password)); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Credenciais inválidas"})
+		return
+	}
+
+	token, err := middlewares.GenerateToken(user.ID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao gerar o token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Login bem-sucedido", "token": token})
 }
