0d6d70ae5c137810c43ca4dfc2b6a7ac5e84af2e
Kevin Gomes <kevinmg50@gmail.com>
2023-11-09 19:39:30 -0300

    feat: new routes and middleware for protection

--- routes/routes.go
+++ routes/routes.go
@@ -5,26 +5,40 @@ import (
 
 	"github.com/gin-gonic/gin"
 	api "github.com/kevinfinalboss/FinOps/api/controller"
+	"github.com/kevinfinalboss/FinOps/api/middlewares"
 	"github.com/kevinfinalboss/FinOps/internal/repository"
+	"github.com/kevinfinalboss/FinOps/internal/services"
 )
 
-func RegisterRoutes(router *gin.Engine, userRepo *repository.UserRepository) {
+func RegisterRoutes(router *gin.Engine, userRepo *repository.UserRepository, spendingRepo *repository.SpendingRepository) {
 	userController := api.NewUserController(userRepo)
+	userService := services.NewUserService(userRepo)
+	spendingService := services.NewSpendingService(spendingRepo)
+	spendingController := api.NewSpendingController(spendingService, userService)
 
-	api := router.Group("/api/v1")
+	apiGroup := router.Group("/api/v1")
 	{
-		api.GET("/healthcheck", healthCheck)
-		api.POST("/user/login", userController.LoginUser)
-		api.POST("/user/register", userController.RegisterUser)
+		apiGroup.GET("/healthcheck", healthCheck)
+		apiGroup.POST("/user/login", userController.LoginUser)
+		apiGroup.POST("/user/register", userController.RegisterUser)
+		apiGroup.POST("/user/register/spendings", spendingController.CreateSpending)
 	}
 
+	authRoutes := router.Group("/")
+	authRoutes.Use(middlewares.AuthMiddleware())
+	{
+		authRoutes.GET("/entradas", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "entradas.html", gin.H{
+				"title": "Entradas Page",
+			})
+		})
+	}
 	router.GET("/login", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "login.html", gin.H{
 			"title": "Login Page",
 		})
 	})
 }
-
 func healthCheck(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "UP"})
 }
