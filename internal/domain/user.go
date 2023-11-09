fd3b042fbcf1503c087c0dbdf3c0c97e421d143b
Kevin Gomes <kevinmg50@gmail.com>
2023-11-09 12:53:23 -0300

    feat: user models

---
+++ internal/domain/user.go
@@ -0,0 +1,13 @@
+package domain
+
+type User struct {
+	ID           string `json:"id" bson:"_id"`
+	FullName     string `json:"full_name" bson:"full_name"`
+	Email        string `json:"email" bson:"email"`
+	PasswordHash string `json:"-" bson:"password_hash"`
+	Address      string `json:"address" bson:"address"`
+	City         string `json:"city" bson:"city"`
+	State        string `json:"state" bson:"state"`
+	ZipCode      string `json:"zip_code" bson:"zip_code"`
+	Country      string `json:"country" bson:"country"`
+}
