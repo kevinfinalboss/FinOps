5bb5aec6d8172f3678958293177aa775a80d4bc1
Kevin Gomes <kevinmg50@gmail.com>
2023-11-13 12:58:39 -0300

    feat: implemented redemption of expenses per month

--- internal/repository/spending_repository.go
+++ internal/repository/spending_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/kevinfinalboss/FinOps/internal/domain"
 	"go.mongodb.org/mongo-driver/bson"
@@ -26,8 +27,27 @@ func (r *SpendingRepository) CreateSpending(ctx context.Context, spending domain
 
 func (r *SpendingRepository) GetRecentSpendings(ctx context.Context) ([]domain.Spending, error) {
 	var spendings []domain.Spending
-	opts := options.Find().SetSort(bson.D{{"created_at", -1}}).SetLimit(3)
+	opts := options.Find().SetSort(bson.D{{Key: "created_at", Value: -1}}).SetLimit(3)
 	cursor, err := r.db.Find(ctx, bson.D{}, opts)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var spending domain.Spending
+		if err := cursor.Decode(&spending); err != nil {
+			return nil, err
+		}
+		spendings = append(spendings, spending)
+	}
+	return spendings, nil
+}
+
+func (r *SpendingRepository) GetSpendingsByMonth(ctx context.Context, month string) ([]domain.Spending, error) {
+	var spendings []domain.Spending
+	regexPattern := fmt.Sprintf("^.*%s.*$", month)
+	query := bson.M{"date": bson.M{"$regex": regexPattern}}
+	cursor, err := r.db.Find(ctx, query)
 	if err != nil {
 		return nil, err
 	}
